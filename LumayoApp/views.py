b5816ea6d49444974ad3462151c5bdc42a14ea6b
Mauricio <mauroalternativo519@gmail.com>
2021-04-17 17:11:28 -0500

    Implementando crear administrador

--- LumayoApp/views.py
+++ LumayoApp/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def Home(request):
-
+    
     return render(request, "LumayoApp/home.html")
+
+
+def crear_administrador(request):
+
+    return render(request, "LumayoApp/crear-administrador.html")
