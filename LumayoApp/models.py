b44d9e02f410377e5038b0d74239db0c36f335f6
Mauricio <mauroalternativo519@gmail.com>
2021-04-18 23:44:48 -0500

    Intentando funcionalidad de login

--- LumayoApp/models.py
+++ LumayoApp/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Clientes(models.Model):
-    nombre=models.CharField(max_length=30)
-    direccion=models.CharField(max_length=50)
+    usuario=models.CharField(max_length=20)
+    contraseña=models.CharField(max_length=20)
+    """ direccion=models.CharField(max_length=50)
     email=models.EmailField()
-    celular=models.CharField(max_length=10)
+    celular=models.CharField(max_length=10) """
+    nombre=models.CharField(max_length=20)
