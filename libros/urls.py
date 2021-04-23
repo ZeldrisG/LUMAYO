254b29504c74bde80ae7524fdf7db24a4d85c7b4
zeldrisg <yoan.lopez@utp.edu.co>
2021-04-22 21:10:29 -0500

    se agrego update y delete libro

--- libros/urls.py
+++ libros/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     path('', views.admin_libro, name="admin_libro"),
     path('add-libro/', views.Add_Libro.as_view(), name="add_libro"),
     path('list-libro/', views.LibroList.as_view(), name="list_libro"),
+    path('<pk>/update-libro/', views.LibroUpdate.as_view(), name="up_libro"),
+    path('<pk>/delete-libro/', views.LibroDelete.as_view(), name="del_libro"),
+
+
 
 
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- libros/views.py
+++ libros/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,HttpResponse
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 
@@ -38,5 +38,22 @@ class LibroList(ListView):
 
 class LibroUpdate(UpdateView):
     model = Libro
+    form_class = AddLibroForm
     template_name = 'libros/actualizar-libro.html'
+    success_url = reverse_lazy('admin_libro')
 
+    def form_valid(self, form):
+        print ('happening2')
+        form.save()
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        print ("form is invalid")
+        print(self.request.FILES)
+        return HttpResponse("form is invalid.. this is just an HttpResponse object")
+
+
+class LibroDelete(DeleteView):
+    model = Libro
+    template_name = 'libros/eliminar-libro.html'
+    success_url = reverse_lazy('list_libro')
