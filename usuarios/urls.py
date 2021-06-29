12e2181ab8ca3770084b9bfb537333255aa6a168
Mauricio <mauroalternativo519@gmail.com>
2021-06-29 16:18:34 -0500

    Reestablecer contraseña listo

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
 
 from usuarios import views
 app_name = 'usuarios'
