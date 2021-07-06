c8b20feeda79e7e5354eae73d86d9180012ffbd8
Mauricio <mauroalternativo519@gmail.com>
2021-07-06 18:55:40 -0500

    Cambiar contraseña listo

--- Lumayo/urls.py
+++ Lumayo/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="LumayoApp/password_reset_sent.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="LumayoApp/password_reset_form.html"), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="LumayoApp/password_reset_done.html"), name = "password_reset_complete"),
+    path('password/', auth_views.PasswordChangeView.as_view(template_name="LumayoApp/password.html"), name = "password"),
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(template_name="LumayoApp/password_change_done.html"), name = "password_change_done"),
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
