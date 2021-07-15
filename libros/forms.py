532dadcc319158df5ee266533ebc2312f36f9669
LubaNarvaez <l.narvaez@utp.edu.co>
2021-07-14 22:29:32 -0500

    Corrigiendo Fecha y ISSN del libro al editar

--- libros/forms.py
+++ libros/forms.py
@@ -12,10 +12,18 @@ class Agregar_Libro_Form(forms.ModelForm):
                         attrs={'class': 'form-control',
                             }),
 
-                    'fec-publicacion':forms.DateInput(
+                    'issn':forms.TextInput(
+                        attrs = {'class': 'form-control',
+                        'placeholder': 'ISSN',
+                        'type':'text',
+                        'id': 'floatingInput',
+
+                        }),
+
+                    'fec_publicacion':forms.DateInput(format=('%Y-%m-%d'),
                         attrs={'class': 'form-control',
                         'placeholder': 'fecha de publicacion',
                         'type':'date',
-                        
+                        'id':'datefield',
                         }),
 }
