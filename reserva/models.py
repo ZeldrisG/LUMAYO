dec4a1d12c11b730db6c1ed957e27af6efbf8606
LubaNarvaez <l.narvaez@utp.edu.co>
2021-07-16 10:17:43 -0500

    añadiendo cantidad maxima de reservas

--- reserva/models.py
+++ reserva/models.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from django.db.models.fields import IntegerField
 import pytz
 
 import django
@@ -7,7 +8,8 @@ from django.utils import timezone
 from Lumayo import settings
 from libros.models import Libro
 
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator, validate_integer
+
 
 
 # Create your models here.
@@ -16,6 +18,12 @@ def get_fec_reserva():
 def get_fec_expiracion():
     return timezone.localtime(timezone.now()) + timedelta(hours=24)
 
+class cantidad_maxima_reserva(IntegerField):
+    validators=[MinValueValidator(1),MaxValueValidator(5)]
+
+def get_cant_max_reserva():
+    return cantidad_maxima_reserva
+    
 class Reserva(models.Model):
     usuario = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     libros = models.ManyToManyField(Libro, through = 'ReservaLibros')
@@ -29,7 +37,7 @@ class ReservaLibros(models.Model):
     fec_reserva = models.DateTimeField(default=django.utils.timezone.now)
     fec_expiracion = models.DateTimeField(default=get_fec_expiracion())
     estado = models.BooleanField(default=True, null=False, blank=False)
-    cantidad = models.IntegerField(default=1 ,null = False, blank=False, validators= [MinValueValidator(1), MaxValueValidator(5)] )
+    cantidad = models.IntegerField(default=get_cant_max_reserva() ,null = False, blank=False)
     
     class Meta:
             ordering = ["-fec_reserva"]
